a121e87d2796049b93f01a00985cd0ec15994be7
Dudnik Kirill <prog.me.2017@gmail.com>
2023-06-27 15:41:00 +0200

    - refactored

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
-use speech_recogn::{run_recogizer_thread, RecogrizerThreadPrefs};
-
 mod speech_recogn;
 
+use std::thread;
+
+use speech_recogn::{create_recogizer_stream, RecognizerStreamPrefs, RecognizerData};
+
+const RUS_MODEL_PATH: &str = "D:/Varia/Projects/RustProjects/test_speech_recogn/vosk-model-small-ru-0.22";
+
 fn main() {
-    let model_path = "D:/Varia/Projects/RustProjects/test_speech_recogn/vosk-model-small-ru-0.22";
-    let prefs = RecogrizerThreadPrefs {
-        lang_model_path: model_path.to_string(),
+    let prefs = RecognizerStreamPrefs {
+        lang_model_path: RUS_MODEL_PATH.to_string(),
         max_alternatives: 10,
         keep_words: false,
         keep_partial_words: false,
     };
-    let stream = run_recogizer_thread(prefs);
-    loop {
+    let (_stream, recognizer_data) = create_recogizer_stream(prefs);
+    thread::spawn(|| main_loop(recognizer_data));
+    loop { }
+}
 
+fn main_loop(data: RecognizerData) {
+    loop {
+        {
+            let mut results_lock = data.results.lock().unwrap();
+            let results = &mut results_lock.results;
+            for (ir, r) in results.drain(..).enumerate() {
+                println!("result ({})", ir);
+                for (i, alt) in r.alternatives.iter().enumerate() {
+                    println!("\t{} ({})", alt, i);
+                }
+            }
+        }
+
+        std::thread::sleep_ms(100);
     }
-    drop(stream);
 }
--- src/speech_recogn.rs
+++ src/speech_recogn.rs
@@ -1,18 +1,52 @@
-use std::{time::Duration, sync::{Arc, Mutex}};
+use std::sync::{Arc, Mutex};
 
 use cpal::{Sample, ChannelCount, traits::{HostTrait, DeviceTrait, StreamTrait}, SampleFormat, Device, SupportedStreamConfig, Stream};
 use dasp::sample::ToSample;
-use vosk::{Recognizer, DecodingState, Model};
+use vosk::{Recognizer, DecodingState, Model, CompleteResultMultiple};
 
 #[derive(Clone)]
-pub struct RecogrizerThreadPrefs {
+pub struct RecognizerStreamPrefs {
     pub lang_model_path: String,
     pub max_alternatives: u16,
     pub keep_words: bool,
     pub keep_partial_words: bool,
-} 
+}
 
-pub fn run_recogizer_thread(prefs: RecogrizerThreadPrefs) -> Stream {
+pub struct RecognizerData {
+    pub results: Arc<Mutex<RecognitionResults>>,
+}
+
+pub struct RecognitionResults {
+    pub results: Vec<RecognitionResult>
+}
+
+pub struct RecognitionResult {
+    pub alternatives: Vec<String>,
+}
+
+impl RecognitionResults {
+    fn new() -> RecognitionResults {
+        RecognitionResults {
+            results: Vec::new(),
+        }
+    }
+}
+
+impl From<CompleteResultMultiple<'_>> for RecognitionResult {
+    fn from(value: CompleteResultMultiple<'_>) -> Self {
+        let mut result = RecognitionResult {
+            alternatives: Vec::with_capacity(value.alternatives.len()),
+        };
+
+        for alt in value.alternatives {
+            result.alternatives.push(alt.text.to_string());
+        }
+
+        result
+    }
+}
+
+pub fn create_recogizer_stream(prefs: RecognizerStreamPrefs) -> (Stream, RecognizerData) {
     let audio_input_device = cpal::default_host()
         .default_input_device()
         .expect("No input device connected");
@@ -30,48 +64,61 @@ pub fn run_recogizer_thread(prefs: RecogrizerThreadPrefs) -> Stream {
     recognizer.set_partial_words(prefs.keep_partial_words);
 
     let recognizer = Arc::new(Mutex::new(recognizer));
-    let stream = create_stream(config, audio_input_device, recognizer.clone(), channels);
+    let results = Arc::new(Mutex::new(RecognitionResults::new()));
+    let stream = create_stream(config, audio_input_device, recognizer.clone(), results.clone(), channels);
 
     stream.play().expect("Could not play stream");
-    stream
+    (
+        stream,
+        RecognizerData {
+            results,
+        }
+    )
 }
 
 pub fn create_stream(
     config: SupportedStreamConfig,
     audio_input_device: Device,
     recognizer: Arc<Mutex<Recognizer>>,
+    results: Arc<Mutex<RecognitionResults>>,
     channels: u16
     ) -> Stream {
     let err_fn = move |err| {
         eprintln!("an error occurred on stream: {}", err);
     };
-    
+
     match config.sample_format() {
         SampleFormat::F32 => audio_input_device.build_input_stream(
             &config.into(),
-            move |data: &[f32], _| recognize(&mut recognizer.lock().unwrap(), data, channels),
+            move |data: &[f32], _| {
+//                println!("");
+//                println!("lock");
+                recognize(&mut recognizer.lock().unwrap(), &mut results.lock().unwrap(), data, channels);
+//                println!("unlock");
+            },
             err_fn,
             None,
         ),
         SampleFormat::U16 => audio_input_device.build_input_stream(
             &config.into(),
-            move |data: &[u16], _| recognize(&mut recognizer.lock().unwrap(), data, channels),
+            move |data: &[u16], _| recognize(&mut recognizer.lock().unwrap(), &mut results.lock().unwrap(), data, channels),
             err_fn,
             None,
         ),
         SampleFormat::I16 => audio_input_device.build_input_stream(
             &config.into(),
-            move |data: &[i16], _| recognize(&mut recognizer.lock().unwrap(), data, channels),
+            move |data: &[i16], _| recognize(&mut recognizer.lock().unwrap(), &mut results.lock().unwrap(), data, channels),
             err_fn,
             None,
         ),
-        _ => todo!(),
+        _ => unreachable!(),
     }
     .expect("Could not build stream")
 }
 
-pub fn recognize<T: Sample + ToSample<i16>>(
+pub fn recognize<T: Sample + ToSample<i16>> (
     recognizer: &mut Recognizer,
+    results: &mut RecognitionResults,
     data: &[T],
     channels: ChannelCount,
     ) {
@@ -89,7 +136,9 @@ pub fn recognize<T: Sample + ToSample<i16>>(
         }
         DecodingState::Finalized => {
             // Result will always be multiple because we called set_max_alternatives
-            println!("result: {:#?}", recognizer.result().multiple().unwrap());
+            println!("res write");
+            results.results.push(recognizer.result().multiple().unwrap().into());
+//            println!("result: {:#?}", recognizer.result().multiple().unwrap());
         }
         DecodingState::Failed => eprintln!("error"),
     }
